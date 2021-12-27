efa8d5663bf726d235f5b5f2a8c485f5662810ba
elaes <traore-lassana@laposte.net>
2021-12-27 09:46:56 +0100

    add email controller

---
+++ controller/emailController.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Howdy !</h3>
+    <span>to : </span><span><?php echo $_POST["to"] ?></span><br>
+    <span>content : </span><p><?php echo $_POST["content"] ?></p>
+</body>
+</html>
