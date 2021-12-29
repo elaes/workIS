e1cc2365f4f175304b9619d604ed98a5c10a8a56
elaes <traore-lassana@laposte.net>
2021-12-29 23:03:50 +0100

    home page with MVC OK

---
+++ app/core/App.php
@@ -0,0 +1,40 @@
+<?php
+
+class App {
+
+    protected $controller = 'home';
+
+    protected $method = 'index';
+
+    protected $params = [];
+    
+    public function __construct(){
+        $url = $this->parseUrl();
+        if( file_exists('../app/controllers/' . $url[0] . '.php') ){
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+        
+        require_once '../app/controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+
+        if( isset($url[1]) ){
+            if( method_exists($this->controller, $url[1]) ){
+                $this->method = $url[1];
+                unset($url[1]);
+            } 
+        }
+
+        $this->params = $url ? array_values($url) : [];
+        call_user_func_array([$this->controller, $this->method], $this->params);
+    }
+
+    public function parseUrl(){
+        $link = $_SERVER['REQUEST_URI'];
+        if( isset($link ) ){
+            return explode('/', (substr($link, 8, strlen($link))));
+        }
+    }
+}
+
+?>
