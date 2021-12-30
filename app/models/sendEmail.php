074e9087f100f73fc2166824a4ae3c2b136a6728
elaes <traore-lassana@laposte.net>
2021-12-30 15:33:32 +0100

    add content for email

--- app/models/sendEmail.php
+++ app/models/sendEmail.php
@@ -22,12 +22,14 @@ $mail->Username   = "lassana.traore2022@gmail.com";
 $mail->Password   = "mwxjshszstfcvbod";
 
 $mail->IsHTML(true);
-$mail->AddAddress("traore_lassana@msn.com", "recipient-name");
-$mail->SetFrom("lassana.traore2022@gmail.com", "from-name");
-$mail->AddReplyTo("lassana.traore2022@gmail.com", "reply-to-name");
-$mail->AddCC("lassana.traore2022@gmail.com", "cc-recipient-name");
+$mail->AddAddress($this->to, $this->to);
+$mail->SetFrom("lassana.traore2022@gmail.com", "Lassana TRAORE");
+$mail->AddReplyTo("lassana.traore2022@gmail.com", "Lassana TRAORE");
+// $mail->AddCC("lassana.traore2022@gmail.com", "cc-recipient-name");
 $mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
-$content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
+ob_start();
+include '../app/views/email/content.php';
+$content = ob_get_clean();
 
 $mail->MsgHTML($content); 
 if(!$mail->Send()) {
---
+++ app/views/email/content.php
@@ -0,0 +1,5 @@
+<table>
+    <h3>My content</h3>
+    <?= $this->content ?>
+    <span>end</span>
+</table>
